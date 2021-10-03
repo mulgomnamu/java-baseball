c7f6ccef5a3753fbd8c10be8e2c339d5a3212f83
mulgomnamu <33394293+mulgomnamu@users.noreply.github.com>
2021-10-03 12:27:12 +0900

    Initial commit

---
+++ java-baseball/src/main/java/com/nextstep/baseball/JavaBaseballApplication.java
@@ -0,0 +1,13 @@
+package com.nextstep.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBaseballApplication.class, args);
+	}
+
+}
---
+++ java-baseball/src/test/java/com/nextstep/baseball/JavaBaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextstep.baseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
